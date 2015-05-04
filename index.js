14ea971a984e83a10585658d7f85650830ed6528
Andrew Hallock <andrew.hallock@gmail.com>
2015-05-04 17:10:50 +0000

    add get-workflow-input to index

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
   filterActivities: require('./lib/filter-activities'),
   filterEvents: require('./lib/filter-events'),
   getAttributes: require('./lib/get-attributes'),
+  getWorkflowInput: require('./lib/get-workflow-input'),
   poll: require('./lib/poll'),
   pollForDecisionTask: require('./lib/poll-for-decision-task')
 };
